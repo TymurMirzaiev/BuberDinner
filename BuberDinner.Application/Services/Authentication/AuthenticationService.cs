bc10dfa2d9444c6b1edbd828d25cbf35d302bdc7
Tymur Mirzaiev <tymi01@betssongroup.com>
2025-04-05 21:26:56 +0300

    add simple user repository

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,4 +1,6 @@
 ﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +12,57 @@ namespace BuberDinner.Application.Services.Authentication
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
 
-        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
         }
 
         public AuthenticationResult Login(string email, string password)
         {
-            return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
+            if(_userRepository.GetUserByEmail(email) is not User user)
+            {
+                throw new Exception("User with given email does not exists.");
+            }
+
+            if(user.Password != password)
+            {
+                throw new Exception("Invalid password.");
+            }
+
+            var token = _jwtTokenGenerator.GenerateToken(user);
+
+            return new AuthenticationResult(
+                user, 
+                token);
         }
 
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
-            // Check if user already exists
+            var user = _userRepository.GetUserByEmail(email);
 
-            // Create user(generate unique ID)
+            if (user is not null)
+            {
+                throw new Exception("User with given email already exists.");
+            }
 
-            // Create JWT token
+            user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Password = password,
+            };
 
-            Guid userId = Guid.NewGuid();
+            _userRepository.Add(user);
 
-            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+            var token = _jwtTokenGenerator.GenerateToken(user);
             
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, token);
+            return new AuthenticationResult(
+                user,
+                token);
         }
     }
 }
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
 using BuberDinner.Application.Common.Services;
 using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Persistence;
 using BuberDinner.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,8 @@ namespace BuberDinner.Infrastructure
 
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
