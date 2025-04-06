080c37e6e7e9c6b1c163f85ccbd4123728d52e8f
Tymur Mirzaiev <tymi01@betssongroup.com>
2025-04-06 15:38:47 +0300

    add Mapster

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,15 +1,16 @@
+using BuberDinner.Api;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
+        .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddControllers();
 }
-
 
 var app = builder.Build();
 {
